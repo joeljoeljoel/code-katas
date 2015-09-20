56f167eaa67ef781b28bb5b71ead5d8dc28b77f9
Joel Jacob <joel@viastudio.com>
2015-09-20 11:33:38 -0400

    Attempting to breakdown a number

--- PrimeFactors/PrimeFactors.php
+++ PrimeFactors/PrimeFactors.php
@@ -1,13 +1,38 @@
 <?php
 class PrimeFactors {
 
-    public $factors;
+    public static $factors = [];
 
-    public static function factor($number) {
-        return self::isPrime($number);
+    public function __construct($number) {
+        $this->factors = [$number];
     }
 
-    private static function isPrime($factor = null) {
+    public function factor() {
+        return $this->breakdown($this->factors);
+    }
+
+    private function breakdown(array $numbers) {
+        foreach ($numbers as $number) {
+            if ($this->isPrime($number)) {
+                continue;
+            }
+
+            $try = 2;
+
+            while ($try < $number) {
+                if ($number % $try === 0) {
+                    $this->factors = array_merge([$try], $this->breakdown([$number / $try]));
+                }
+                $try++;
+            }
+
+        }
+
+
+        return $this->factors;
+    }
+
+    private function isPrime($factor = null) {
         $try = 2;
 
         while ($try < $factor) {
@@ -21,3 +46,7 @@ class PrimeFactors {
     }
 
 }
+
+$factor = new PrimeFactors(20);
+$factors = $factor->factor();
+var_dump($factors);
